c0f85ce51d571b903ce0a276fc971f366e625220
Pablo Frank <frankpablo@gmail.com>
2025-02-12 13:28:49 -0500

    added hw02_main.py

---
+++ hw02_main.py
@@ -0,0 +1,49 @@
+"""
+Name: (put your name here)
+Peers: (add any collaborators)
+References: (anything you checked to solve this)
+"""
+
+# Task 1.1:
+#  Complete the function "read_two_ints" below:
+def read_two_ints():
+  return 1, 2
+
+# Task 2.1:
+#  Complete the function "compute_multadd" below:
+def compute_multadd(a, b):
+  pass
+
+# Task 3.1:
+#  Complete the function "print_fancy" below:
+def print_fancy(a, b, ab_multadd):
+  pass
+
+def main ():
+  # Task 1.2:
+  #  Add one line below to call read_two_ints
+  #  with no arguments
+  #  store the returned values into two variables: x and y
+
+  # TODO: add your call instead of this line
+
+  # Task 2.2:
+  #  Add one line below to call multadd
+  #  with arguments x, and y;
+  #  store the returned value in "xy_multadd"
+
+  # TODO: add your call instead of this line
+
+  # Task 3.2:
+  #  Complete The line below to call print_fancy
+  #  with arguments x, y, and xy_multadd;
+
+  # TODO: add your call instead of this line
+
+
+  # Do not modify this final print statement
+  print("The End")
+
+# Do not modify these two lines
+if __name__ == "__main__":
+  main()
