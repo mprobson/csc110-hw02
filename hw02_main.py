8ec00c293c3bde5480517d70e7b445c922c4d4a1
Pablo Frank <frankpablo@gmail.com>
2025-02-13 12:57:44 -0500

    Update hw02_main.py

--- hw02_main.py
+++ hw02_main.py
@@ -7,36 +7,42 @@ References: (anything you checked to solve this)
 # Task 1.1:
 #  Complete the function "read_two_ints" below:
 def read_two_ints():
+  # the return shown below is a placeholder to make sure this runs
+  # TODO: complete the function instead of the line shown below
   return 1, 2
 
 # Task 2.1:
 #  Complete the function "compute_multadd" below:
 def compute_multadd(a, b):
+  # the pass shown below is a placeholder to make sure this runs
+  # TODO: complete the function instead of the line shown below
   pass
 
 # Task 3.1:
 #  Complete the function "print_fancy" below:
 def print_fancy(a, b, ab_multadd):
+  # the pass shown below is a placeholder to make sure this runs
+  # TODO: complete the function instead of the line shown below
   pass
 
 def main ():
   # Task 1.2:
-  #  Add one line below to call read_two_ints
-  #  with no arguments
+  #  Add one line below to call read_two_ints (note that it returns two values)
+  #  the call should provide no arguments
   #  store the returned values into two variables: x and y
 
   # TODO: add your call instead of this line
 
   # Task 2.2:
-  #  Add one line below to call multadd
-  #  with arguments x, and y;
-  #  store the returned value in "xy_multadd"
+  #  Add one line below to call multadd (note that it returns one value)
+  #  the call should provide the arguments x, and y you obtained above;
+  #  store the returned value in a variable called xy_multadd
 
   # TODO: add your call instead of this line
 
   # Task 3.2:
   #  Complete The line below to call print_fancy
-  #  with arguments x, y, and xy_multadd;
+  #  the call should provide the arguments x, y, and xy_multadd you obtained above;
 
   # TODO: add your call instead of this line
 
